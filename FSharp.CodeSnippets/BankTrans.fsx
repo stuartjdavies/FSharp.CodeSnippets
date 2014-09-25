1bb2e49accadf252cdd7aa4247d5027149e0c181
stuartjdavies <stuart_j_davies@hotmail.com>
2014-09-25 13:51:32 +1000

    Added new scripts

--- FSharp.CodeSnippets/BankTrans.fsx
+++ FSharp.CodeSnippets/BankTrans.fsx
@@ -1,2 +1,376 @@
-﻿
+﻿#load "../packages/FsLab.0.0.19/FsLab.fsx"
+#r "Microsoft.Office.Interop.Excel.dll"
+
+open FSharp.Data
+open System
+open Deedle
+open FSharp.Charting
+open Microsoft.Office.Interop.Excel
+
+let transFileName = "testtrans.csv"
+
+type BankTrans = { Date : DateTime; PaymentType : String; 
+                   Description : String; Category : String; 
+                   Debit : Decimal; Credit : Decimal; 
+                   Balance : Decimal; }
+
+module ImportBankTrans = 
+         type Transactions = CsvProvider<".\\testtrans.csv">
+
+         let GetPaymentType(s : string) = 
+                if (s.Length > 30) then
+                  s.Substring(0, 30).Trim()
+                else
+                  String.Empty  
+
+         let GetDesc(s : string) =
+                if (s.Length > 30) then         
+                  s.Substring(s.IndexOf(" ", 30)).Trim()
+                else
+                  s         
+         
+         let MapCategory(t : Transactions.Row) =
+                if (t.Description.Contains("Woolworths")) then
+                    "Supermarket"
+                else if (t.Description.Contains("Carshare")) then
+                    "Carshare"
+                else if (t.Description.Contains("Big W")) then
+                    "Big W"
+                else if (t.Description.Contains("Myki")) then
+                    "Myki"                         
+                else if (t.Description.Contains("Primus")) then
+                    "Primus"
+                else if (t.Description.Contains("Hcf")) then
+                    "Hcf"
+                else if (t.Description.Contains("Aldi")) then
+                    "Aldi"
+                else if (t.Description.Contains("Rent")) then                        
+                    "Rent"
+                else
+                    GetDesc(t.Description)
+       
+         let ImportCsv(fileName : string) = 
+                let rawTs = Transactions.Load(fileName)
+
+                rawTs.Rows 
+                |> Seq.filter(fun t -> t.Date <> "")                       
+                |> Seq.map(fun t ->                                
+                    { Date = DateTime.Parse(t.Date);
+                      PaymentType = GetPaymentType(t.Description);  
+                      Description = GetDesc(t.Description);                                 
+                      Category = MapCategory(t);
+                      Debit = t.Debit;
+                      Credit = t.Credit;
+                      Balance = t.Balance }) 
+                |> Seq.sortBy(fun t -> t.Date)                      
+              
+module TransStats = 
+        let private GetWeek(d : DateTime) =
+             let day = (int d.DayOfWeek)
+             String.Format("{0} - {1}", 
+                           d.AddDays((float (1 - day))).ToShortDateString(), 
+                           d.AddDays((float (7 - day))).ToShortDateString())
+
+        let debits(ts : seq<BankTrans>) =
+            ts |> Seq.filter(fun t -> t.Debit <> 0.0m)                               
+        
+        let credits(ts : seq<BankTrans>) =
+            ts |> Seq.filter(fun t -> t.Credit <> 0.0m)        
+        
+        let maxDebit(ts : seq<BankTrans>) = 
+            ts |> Seq.maxBy(fun t -> t.Debit)
+         
+        let minDebit(ts : seq<BankTrans>) = 
+            ts |> debits
+               |> Seq.minBy(fun t -> t.Debit)
+         
+        let avgDebit(ts : seq<BankTrans>) =            
+            ts |> debits
+               |> Seq.averageBy(fun t -> t.Debit)        
+        
+        let maxCredit(ts : seq<BankTrans>) = 
+            ts |> credits
+               |> Seq.maxBy(fun t -> t.Credit)
+         
+        let minCredit(ts : seq<BankTrans>) = 
+            ts |> Seq.minBy(fun t -> t.Credit)
+
+        let avgCredit(ts : seq<BankTrans>) = 
+            ts |> credits
+               |> Seq.averageBy(fun t -> t.Credit)        
+                        
+        let count(ts : seq<BankTrans>) = 
+            ts |> Seq.length
+            
+        let minBalance(ts : seq<BankTrans>) =
+            ts |> Seq.minBy(fun t -> t.Balance)
+
+        let maxBalance(ts : seq<BankTrans>) =
+            ts |> Seq.maxBy(fun t -> t.Balance)
+
+        let totalCredits(ts : seq<BankTrans>) = 
+            ts |> Seq.sumBy(fun t -> t.Credit)
+
+        let totalDebits(ts : seq<BankTrans>) = 
+            ts |> Seq.sumBy(fun t -> t.Debit)
+
+        let totalDiff(ts : seq<BankTrans>) = 
+            totalCredits(ts) - totalCredits(ts)
+        
+        let debitTotalsByCategory(ts : seq<BankTrans>, 
+                                  category : string) =                         
+            ts |> Seq.groupBy(fun t -> t.Category)
+               |> Seq.map(fun (d, ts) -> 
+                   (d,(ts |> Seq.sumBy(fun t -> t.Debit))))
+
+        let creditTotalsByCategory(ts : seq<BankTrans>) =
+            ts |> Seq.groupBy(fun t -> t.Category)
+               |> Seq.map(fun (d, ts) ->
+                              (d,(ts |> Seq.sumBy(fun t -> t.Credit))))
+
+        let dailyDebits(ts : seq<BankTrans>) =
+            ts |> Seq.groupBy(
+                    fun t -> t.Date.ToShortDateString())
+               |> Seq.map(fun (d, ts) ->
+                    (d,(ts |> Seq.sumBy(fun t -> t.Debit))))
+
+        let dailyCredits(ts : seq<BankTrans>) =
+                ts |> Seq.groupBy(
+                        fun t -> (t.Date.Day, t.Date.Month, t.Date.Year))
+                   |> Seq.map(
+                        fun (d, ts) ->
+                            (d,(ts |> Seq.sumBy(fun t -> t.Credit))))
+
+        let monthlyDebits(ts : seq<BankTrans>) = 
+                ts |> Seq.groupBy(
+                        fun t -> t.Date.ToShortDateString())                                                       
+                   |> Seq.map(
+                        fun (d, ts) ->
+                            (d,(ts |> Seq.sumBy(fun t -> t.Debit))))       
+
+        let monthlyCredits(ts : seq<BankTrans>) = 
+                ts |> Seq.groupBy(
+                        fun t -> (t.Date.Month, t.Date.Year))                                                       
+                   |> Seq.map(
+                        fun (d, ts) ->
+                            (d,(ts |> Seq.sumBy(fun t -> t.Credit))))       
+
+        let weeklyDebitTotals (ts : seq<BankTrans>) =
+                ts |> Seq.groupBy(
+                         fun t -> GetWeek(t.Date))
+                   |> Seq.map(fun (d, ts) ->
+                         (d,(ts |> Seq.sumBy(fun t -> t.Debit))))
+
+        let weeklyCreditTotals(ts : seq<BankTrans>) = 
+                ts |> Seq.groupBy(
+                         fun t -> GetWeek(t.Date))
+                   |> Seq.map(fun (d, ts) ->
+                         (d,(ts |> Seq.sumBy(fun t -> t.Credit))))
+
+        let debitsByCategory(ts : seq<BankTrans>) = 
+                ts |> Seq.filter(
+                         fun t -> t.Debit <> 0.0m)
+                   |> Seq.groupBy(
+                         fun t -> t.Category)
+                   |> Seq.map(
+                         fun (d, ts) ->
+                         (d,(ts |> Seq.sumBy(fun t -> t.Debit))))                           
+
+        let creditsByCategory(ts : seq<BankTrans>) = 
+                ts |> Seq.filter(
+                         fun t -> t.Credit <> 0.0m)
+                   |> Seq.groupBy(
+                         fun t -> t.Category)
+                   |> Seq.map(
+                         fun (d, ts) ->
+                            (d,(ts |> Seq.sumBy(fun t -> t.Credit))))
+
+        let first(ts : seq<BankTrans>) =
+                (ts |> Seq.sort |> Seq.head)
+        
+        let last(ts : seq<BankTrans>) =
+                (ts |> Seq.sort |> Seq.last)
+
+        let summary(ts : seq<BankTrans>) =                       
+                [["Number of transactions" :> obj; (count(ts) :> obj)];                          
+                 ["Start date"; (first(ts).Date.ToShortDateString() :> obj)];
+                 ["Finish date"; (last(ts).Date.ToShortDateString())];
+                 ["Starting balance"; (first(ts).Balance)];
+                 ["Ending balance"; (last(ts).Balance :> obj)];                           
+                 ["Number of Debits"; (ts |> debits |> count)];           
+                 ["Max Debit"; (ts |> maxDebit).Debit];
+                 ["Min Debit"; (ts |> minDebit).Debit];
+                 ["Avg Debit"; (ts |> avgDebit)];
+                 ["Total Debits"; (totalDebits(ts))]; 
+                 ["Number of Credits"; (ts |> credits |> count)];
+                 ["Max Credit"; (ts |> maxCredit).Credit]; 
+                 ["Min Credit"; (ts |> minCredit).Credit];
+                 ["Avg Credit"; (ts |> avgCredit)];
+                 ["Total Credits"; (totalCredits(ts))];
+                 ["Net(Credits - Debits)"; (ts |> totalDiff)]];        
+                          
+        let printSummary(ts : seq<BankTrans>) =
+                let c = count(ts)
+                printfn "Number of transactions %i" (count(ts))                          
+                printfn "Start date - %s" (first(ts).Date.ToShortDateString())
+                printfn "Finish date - %s" (last(ts).Date.ToShortDateString())
+                printfn "Starting balance - %f" (first(ts).Balance)
+                printfn "Ending balance - %f" (last(ts).Balance)            
+               
+                printfn "Number of Debits %d" (ts |> debits |> count)           
+                printfn "Max Debit - $%0.0f" (ts |> maxDebit).Debit
+                printfn "Min Debit - $%0.0f" (ts |> minDebit).Debit
+                printfn "Avg Debit - $%0.0f" (ts |> avgDebit)
+                printfn "Total Debits - %f" (totalDebits(ts)) 
+
+                printfn "Number of Credits %d" (ts |> credits |> count)
+                printfn "Max Credit - $%0.0f" (ts |> maxCredit).Credit 
+                printfn "Min Credit - $%0.0f" (ts |> minCredit).Credit
+                printfn "Avg Credit - $%0.0f" (ts |> avgCredit)
+                printfn "Total Credits - %f" (totalCredits(ts)) 
+
+                printfn "Net(Credits - Debits) - $%0.0f" (ts |> totalDiff)
+                printfn "Trans count - %i" (count(ts))
+
+        let PrintTopDebitsByCat(n, ts : seq<BankTrans>) =                
+                ts |> debitsByCategory                                                                   
+                   |> Seq.sortBy(fun (c,t) -> -t)
+                   |> Seq.take n                     
+                   |> Seq.iter(fun (c,t) -> 
+                              printfn "%s - $%0.0f" c t
+                              ())
+
+        let PrintTopCreditsByCat(n, ts : seq<BankTrans>) =                
+                ts |> creditsByCategory                                                                   
+                   |> Seq.sortBy(fun (c,t) -> -t)
+                   |> Seq.take n                     
+                   |> Seq.iter(fun (c,t) -> 
+                                   printfn "%s - $%0.0f" c t
+                                   ())                        
+
+module ExpensesReport =
+    let CreateReport(ts : seq<BankTrans>) =
+           let app = new ApplicationClass(Visible = true) 
+           let workbook = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet)                       
+           workbook.Worksheets.Add() |> ignore
+           workbook.Worksheets.Add() |> ignore
+           workbook.Worksheets.Add() |> ignore
+           workbook.Worksheets.Add() |> ignore
+           
+           let summaryWorksheet = (workbook.Worksheets.[1] :?> Worksheet)            
+           summaryWorksheet.Name <- "Summary"         
+           let weeklySummaryWorksheet = (workbook.Worksheets.[2] :?> Worksheet)
+           weeklySummaryWorksheet.Name <- "Weekly Summary"
+           let monthlySummaryWorksheet = (workbook.Worksheets.[3] :?> Worksheet)
+           monthlySummaryWorksheet.Name <- "Monthly Summary"           
+           let transWorksheet = (workbook.Worksheets.[4] :?> Worksheet)
+           transWorksheet.Name <- "Transactions"         
+
+           let titles = [| "Date";"PaymentType"; 
+                           "Description";"Category"; 
+                           "Debit"; "Credit"; 
+                           "Balance" |]                                   
+           transWorksheet.Range("A1", "F1").Value2 <- titles                                        
+           
+           let data = ts |> Seq.map(fun t -> 
+                                [(t.Date.ToString() :> obj);
+                                 (t.PaymentType.ToString() :> obj);
+                                 (t.Category :> obj);
+                                 (t.Credit :> obj);
+                                 (t.Debit :> obj);
+                                 (t.Balance :> obj);])
+                         |> array2D           
+           
+           let numberOfTrans = Seq.length(ts)       
+                                                                                                                       
+           transWorksheet.Range("A2", String.Format("F{0}", 
+                                 Seq.length(ts)))
+                         .Value2 <- data 
+         
+           let summaryData = ts |> TransStats.summary
+                                |> array2D
+                
+           let kpvs = ts |> TransStats.summary |> Seq.length
+
+           summaryWorksheet.Range("A1","A1").Value2 <- "Summary"           
+           summaryWorksheet.Range("A3", 
+                                  String.Format("B{0}", 
+                                           kpvs))
+                           .Value2 <- summaryData
+           
+           // Weekly           
+           //let ts |> TransStats.weeklyDebitTotals
+           //       |> array2D
+           //ts |> TransStats.weeklyCreditTotals
+           //   |> array2D
+
+           //weeklySummaryWorksheet.Range("A2", )
+           
+           // Monthly
+           //monthlySummaryWorksheet
+
+
+
+
+           //["Total"; ""; "", "=SUM(A1:"; "=SI< ] 
+
+           //worksheet.Range()
+
+           // 2. Write Transactions By Date 
+           // 3. How much was spent each week
+           // 4. How much was spent per month
+           // 5. Write Transactions By Debit.
+           // 6. Order Transactions By Credits           
+           // 9. Write Bar Graph of top 10 Debits
+           // 10. Write Bar Graph of top 10 Credits
+           // 11. 
+                                                              
+let trans = ImportBankTrans.ImportCsv(transFileName)
+
+ExpensesReport.CreateReport(trans)
+
+trans |> TransStats.debitsByCategory      
+      |> Seq.sortBy (fun (d,t) -> -t)
+      |> Seq.take 6
+      |> Chart.Pie
+
+trans |> TransStats.monthlyDebits
+      |> Chart.Bar
+
+trans |> TransStats.summary
+
+           
+
+// Load "Asde"
+// Display debit transactions greater_than 10
+// Display debit transactions greater_than 20
+// Create piechart where transactions greater_than 30
+// Display_top 5 debits
+// Display_last 3 debits
+// Display_summary"
+// 
+
+
+//trans |> PrintTransBy fun t ->
+
+//1. Debits
+//2. Credits
+//3. PrintBy
+//4. PrintGreaterThan trans
+//5. PrintLessThan 
+//6. PrintPieGraph 
+//7. PrintLineGraph
+
+// 2. Pie char of debits
+// 4. Pie chart of credits
+// 6. Line graph
+// 8. Line graph
+// 9. Payment type count
+// 10. Payment type pie chart
+
+
+//for (desc,d,c) in catTotals do
+   
+  // printfn "%s %f %f" desc d c
+                        
 
---
+++ FSharp.CodeSnippets/GraphEmissions.fsx
@@ -0,0 +1,66 @@
+﻿#r @"..\packages\R.NET.Community.1.5.15\lib\net40\RDotNet.dll"
+#r @"..\packages\R.NET.Community.FSharp.0.1.8\lib\net40\RDotNet.FSharp.dll"
+#r @"..\packages\RProvider.1.0.17\lib\net40\RProvider.dll"
+#r @"..\packages\RProvider.1.0.17\lib\net40\RProvider.Runtime.dll"
+#r @"..\packages\ExcelDataReader.2.1\lib\net20\Excel.dll"
+#r @"..\packages\FSharp.Data.2.0.15\lib\net40\FSharp.Data.dll"
+#r @"..\packages\FSharpx.TypeProviders.Excel.1.8.41\lib\40\FSharpx.TypeProviders.Excel.dll"
+#r @"FSharp.Data.TypeProviders.dll" 
+
+open System
+open RDotNet
+open RProvider
+open FSharp.Data
+open RProvider.``base``
+open RProvider.graphics
+open System.Collections.Generic
+
+open FSharpx.TypeProviders.Excel
+open FSharpx
+open System.IO
+open Excel
+open Microsoft.FSharp.Data.TypeProviders
+open System.Linq
+open System.Data
+
+let filePath = @".\NGERS-2009-10.xlsx"
+type EmissionsExcelSchema = ExcelFile<".\NGERS-2009-10.xlsx">
+let file = new EmissionsExcelSchema(filePath)
+
+let rows = file.Data                 
+           |>  Seq.map(fun r -> 
+                        try
+                            (r.``Registered Corporations``,
+                             r.``Total scope 1 greenhouse gas emissions (t CO2-e) ``,
+                             r.``Total scope 2 greenhouse gas emissions (t CO2-e) ``,
+                             r.``Total energy consumption (GJ)``)
+                        with                           
+                           | _ -> (String.Empty,0.0,0.0,0.0))                              
+           |> Seq.filter(fun (d,_,_,_) -> d <> String.Empty)
+           |> Seq.toList            
+                                                                                                                                                                  
+let e1Total = rows |> Seq.sumBy(fun (d,e1,e2,ec) -> e1)
+let e2Total = rows |> Seq.sumBy(fun (d,e1,e2,ec) -> e2)
+let e3Total = rows |> Seq.sumBy(fun (d,e1,e2,ec) -> ec) 
+
+let topPolluters = rows |> List.sortBy(fun (_,_,_,ec) -> ec)
+                        |> List.rev
+                        |> Seq.ofList
+                        |> Seq.take 5                         
+
+let xs = topPolluters |> Seq.map(fun (_,_,_,ec) -> Convert.ToDouble(ec))
+let ys = topPolluters |> Seq.map(fun (d,_,_,_) -> d)
+
+R.par(namedParams [
+            "mar", box [7;6;4;2];
+            "las", box 1])
+
+R.barplot(namedParams [ 
+            "height", box xs;
+            "beside", box true;
+            "legend", box ys;
+            "main", box "Total energy consumption (GJ)";                  
+            "col", box ["darkblue";"green";"red";"orange";"grey"]])
+            
+R.title(xlab="Registered Corporations", line=2) 
+R.title(ylab="GJ", line=4)                  
