d865603f686a53e545c49032d02d715cd8363fb5
stuartjdavies <stuart_j_davies@hotmail.com>
2014-09-25 12:18:34 +1000

    Added a unemployment script

---
+++ FSharp.CodeSnippets/UnemploymentTrends.fsx
@@ -0,0 +1,79 @@
+﻿#r @"..\packages\R.NET.Community.1.5.15\lib\net40\RDotNet.dll"
+#r @"..\packages\R.NET.Community.FSharp.0.1.8\lib\net40\RDotNet.FSharp.dll"
+#r @"..\packages\RProvider.1.0.17\lib\net40\RProvider.dll"
+#r @"..\packages\RProvider.1.0.17\lib\net40\RProvider.Runtime.dll"
+#r @"..\packages\FSharp.Data.2.0.15\lib\net40\FSharp.Data.dll"
+ 
+open System
+open RDotNet
+open RProvider
+open FSharp.Data
+open RProvider.``base``
+open RProvider.graphics
+open System.Collections.Generic
+
+let wb = WorldBankData.GetDataContext()
+
+let ausEmploy = [ for y in 2000 .. 2014 -> wb.Countries.Australia.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]]
+let italyEmploy = [ for y in 2000 .. 2014 -> wb.Countries.Italy.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+let germanyEmploy = [ for y in 2000 .. 2014 -> wb.Countries.Germany.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+let usEmploy = [ for y in 2000 .. 2014 -> wb.Countries.``United States``.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+let ukEmploy = [ for y in 2000 .. 2014 -> wb.Countries.``United Kingdom``.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+let spainEmploy = [ for y in 2000 .. 2014 -> wb.Countries.Spain.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+let norwayEmploy = [ for y in 2000 .. 2014 -> wb.Countries.Norway.Indicators.``Unemployment, total (% of total labor force) (national estimate)``.[y]] 
+
+let g_range = R.range(0, ausEmploy, italyEmploy)   
+
+R.plot(namedParams [   
+        "x", box ausEmploy; 
+        "type", box "o"; 
+        "col", box "blue";        
+        "ylim", box [| 0; 30|];
+        "ann", box false;])
+
+R.box()
+
+R.lines(namedParams [   
+            "x", box italyEmploy; 
+            "type", box "o";
+            "pch", box 22;  
+            "lty", box 2 
+            "col", box "red";])
+
+R.lines(namedParams [   
+            "x", box germanyEmploy; 
+            "type", box "o"; 
+            "col", box "green";])
+
+R.lines(namedParams [   
+            "x", box usEmploy; 
+            "type", box "o"; 
+            "col", box "purple";])
+
+R.lines(namedParams [   
+            "x", box spainEmploy; 
+            "type", box "o"; 
+            "col", box "grey";])
+
+R.lines(namedParams [   
+            "x", box ukEmploy; 
+            "type", box "o"; 
+            "col", box "brown";])
+
+R.lines(namedParams [   
+            "x", box norwayEmploy; 
+            "type", box "o"; 
+            "col", box "orange";])
+
+R.title(main="Unemployment, total (% of total labor force) (national estimate)")               
+R.title(xlab="year")
+R.title(ylab="% of total labor force")
+
+R.legend("topleft",
+         legend=[|"Aus";"Italy";"Germany";"Us";"Spain";"Uk";"Norway"|],
+         col=[|"blue";"red";"green";"purple";"grey";"brown";"orange"|],
+         lty=[|1;1|],
+         ncol=3)
+
+
+
---
+++ FSharp.CodeSnippets/WriteExcelFile.fsx
@@ -0,0 +1,39 @@
+﻿// Reference the Excel interop assembly
+#r "Microsoft.Office.Interop.Excel.dll"
+open Microsoft.Office.Interop.Excel
+open System
+
+// Run Excel as a visible application
+let app = new ApplicationClass(Visible = true) 
+
+// Create new file and get the first worksheet
+let workbook = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet) 
+// Note that worksheets are indexed from one instead of zero
+let worksheet = (workbook.Worksheets.[1] :?> Worksheet)
+
+// Store data in arrays of strings or floats
+let rnd = new Random()
+let titles = [| "No"; "Maybe"; "Yes" |]
+let names = Array2D.init 10 1 (fun i _ -> string('A' + char(i)))
+let data = Array2D.init 10 3 (fun _ _ -> rnd.NextDouble())
+
+// Populate data into Excel worksheet
+worksheet.Range("C2", "E2").Value2 <- titles
+worksheet.Range("B3", "B12").Value2 <- names
+worksheet.Range("C3", "E12").Value2 <- data
+
+// Add new item to the charts collection
+let chartobjects = (worksheet.ChartObjects() :?> ChartObjects) 
+let chartobject = chartobjects.Add(400.0, 20.0, 550.0, 350.0) 
+
+// Configure the chart using the wizard
+chartobject.Chart.ChartWizard
+  ( Title = "Stacked column chart", 
+    Source = worksheet.Range("B2", "E12"),
+    Gallery = XlChartType.xl3DColumnStacked100, 
+    PlotBy = XlRowCol.xlColumns)
+
+// Set graphical style of the chart
+chartobject.Chart.ChartStyle <- 2
+
+
