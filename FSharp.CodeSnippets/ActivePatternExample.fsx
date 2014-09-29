bc5248995f1ccd8a46863f5c44b4d2184b5a8c34
stuartjdavies <stuart_j_davies@hotmail.com>
2014-09-29 11:42:54 +1000

    Added active patterm matching example

---
+++ FSharp.CodeSnippets/ActivePatternExample.fsx
@@ -0,0 +1,25 @@
+﻿// 
+// I like this feature and will come up with my own example of this. 
+// This is from MSDN at the moment.
+//
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let (|Float|_|) (str: string) =
+   let mutable floatvalue = 0.0
+   if System.Double.TryParse(str, &floatvalue) then Some(floatvalue)
+   else None
+
+let parseNumeric str =
+   match str with
+     | Integer i -> printfn "%d : Integer" i
+     | Float f -> printfn "%f : Floating point" f
+     | _ -> printfn "%s : Not matched." str
+
+parseNumeric "1.1"
+parseNumeric "0"
+parseNumeric "0.0"
+parseNumeric "10"
+parseNumeric "Something else"
