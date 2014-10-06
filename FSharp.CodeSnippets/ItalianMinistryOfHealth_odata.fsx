5198fe79c6f96ce6cb65d8620e6c94290f752a41
stuartjdavies <stuart_j_davies@hotmail.com>
2014-10-07 00:02:34 +1100

    Added italian ministry of health odata

---
+++ FSharp.CodeSnippets/ItalianMinistryOfHealth_odata.fsx
@@ -0,0 +1,29 @@
+﻿#r "System.Data.Linq.dll"
+#r "System.Data.Entity.dll"
+#r "FSharp.Data.TypeProviders.dll"
+#r "System.Data.Services.Client.dll"
+
+open System.Data.Linq
+open System.Data.Entity
+open Microsoft.FSharp.Data.TypeProviders
+open System
+open System.Data
+open System.Data.SqlClient
+open System.Data.EntityClient
+open System.Data.Metadata.Edm
+
+// Italy Ministry of Health Open Data Portal
+type ItalianMinstryOfHealthService = ODataService<"http://opendatasalutedata.cloudapp.net/v1/datacatalog/">
+let db = ItalianMinstryOfHealthService.GetDataContext()
+
+query {
+    for dispositivo in db.DispositiviMedici do
+    select(dispositivo)
+} 
+|> Seq.take 5 
+|> Seq.iter (fun d -> printfn "Description - %s, Type - %s" d.descrizionecnd d.tipo) 
+
+
+
+
+
