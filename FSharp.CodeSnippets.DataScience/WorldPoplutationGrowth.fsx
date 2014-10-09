282338b921b9d464e4d4f6d24c60d8c27504cdd5
stuartjdavies <stuart_j_davies@hotmail.com>
2014-10-10 10:58:04 +1100

    Changed to world population growth

--- FSharp.CodeSnippets.DataScience/WorldPoplutationGrowth.fsx
+++ FSharp.CodeSnippets.DataScience/WorldPoplutationGrowth.fsx
@@ -11,10 +11,10 @@ let wb = WorldBankData.GetDataContext()
 let countries = wb.Countries
  
 let pop2000 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2000]]
-let pop2010 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2010]]
-let surface = series [ for c in countries -> c.Code => c.Indicators.``Surface area (sq. km)``.[2010]]
+let pop2013 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2013]]
+let surface = series [ for c in countries -> c.Code => c.Indicators.``Surface area (sq. km)``.[2013]]
  
-let df = frame [ "Pop2000" => pop2000; "Pop2010" => pop2010; "Surface" => surface ]
+let df = frame [ "Pop2000" => pop2000; "Pop2013" => pop2013; "Surface" => surface ]
 df?Codes <- df.RowKeys
  
 open RProvider.rworldmap 
@@ -22,8 +22,8 @@ open RProvider.rworldmap
 let map = R.joinCountryData2Map(df,"ISO3","Codes")
 R.mapCountryData(map,"Pop2000") 
 
-df?Density <- df?Pop2010 / df?Surface
-df?Growth <- (df?Pop2010 - df?Pop2000) / df?Pop2000
+df?Density <- df?Pop2013 / df?Surface
+df?Growth <- (df?Pop2013 - df?Pop2000) / df?Pop2000
  
 let map2 = R.joinCountryData2Map(df,"ISO3","Codes")
 R.mapCountryData(map2,"Density")
