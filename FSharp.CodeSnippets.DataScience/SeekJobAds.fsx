5084cf97df5caa6ee0166a11c6f395db7412145c
stuartjdavies <stuart_j_davies@hotmail.com>
2014-10-15 00:50:56 +1100

    Added Migrations stuff

---
+++ FSharp.CodeSnippets.DataScience/SeekJobAds.fsx
@@ -0,0 +1,31 @@
+﻿#load "../packages/FsLab.0.0.19/FsLab.fsx"
+#r @"..\packages\ExcelProvider.0.1.2\lib\net40\ExcelProvider.dll"
+#r @"FSharp.Data.TypeProviders.dll" 
+
+open System
+open RDotNet
+open RProvider
+open FSharp.Data
+open RProvider.``base``
+open RProvider.graphics
+open System.Collections.Generic
+open FSharp.Charting
+
+open System.IO
+open FSharp.ExcelProvider
+open Microsoft.FSharp.Data.TypeProviders
+open System.Linq
+open System.Data
+
+//
+// Statistics of Highly skilled visas approved in 2014
+//
+let filePath = @"C:\Users\stuart\Documents\GitHub\FSharp.CodeSnippets\FSharp.CodeSnippets.Data\FA140301378-3.xls"
+type ApplicationsSchema = ExcelFile<"C:\Users\stuart\Documents\GitHub\FSharp.CodeSnippets\FSharp.CodeSnippets.Data\FA140301378-3.xls", "2012-13!A8:F65536">
+
+let applications = new ApplicationsSchema(filePath)
+
+let applicationsApprovedByIndustry = applications.Data |> Seq.filter(fun r -> r.``Nomination Approved``.CompareTo("Y") = 0)
+                                                       |> Seq.groupBy(fun r -> r.``Sponsor Industry (self identified)``)
+                                                       |> Seq.map(fun (g, rows) -> (g, (rows |> Seq.length)))
+                                                       |> Seq.sortBy(fun (g, l) -> -l)
--- FSharp.CodeSnippets.DataScience/WorldPoplutationGrowth.fsx
+++ FSharp.CodeSnippets.DataScience/WorldPoplutationGrowth.fsx
@@ -1,5 +1,9 @@
 ﻿#load "../packages/FsLab.0.0.19/FsLab.fsx"
  
+// This example is lifted from 
+// http://www.clear-lines.com/blog/post/Create-maps-using-R-Deedle-and-FSharp-type-providers.aspx
+// I will create another example with this.
+
 open FSharp.Data
 open RProvider
 open RProvider.``base``
@@ -11,10 +15,10 @@ let wb = WorldBankData.GetDataContext()
 let countries = wb.Countries
  
 let pop2000 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2000]]
-let pop2013 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2013]]
-let surface = series [ for c in countries -> c.Code => c.Indicators.``Surface area (sq. km)``.[2013]]
+let pop2010 = series [ for c in countries -> c.Code => c.Indicators.``Population, total``.[2010]]
+let surface = series [ for c in countries -> c.Code => c.Indicators.``Surface area (sq. km)``.[2010]]
  
-let df = frame [ "Pop2000" => pop2000; "Pop2013" => pop2013; "Surface" => surface ]
+let df = frame [ "Pop2000" => pop2000; "Pop2010" => pop2010; "Surface" => surface ]
 df?Codes <- df.RowKeys
  
 open RProvider.rworldmap 
@@ -22,8 +26,8 @@ open RProvider.rworldmap
 let map = R.joinCountryData2Map(df,"ISO3","Codes")
 R.mapCountryData(map,"Pop2000") 
 
-df?Density <- df?Pop2013 / df?Surface
-df?Growth <- (df?Pop2013 - df?Pop2000) / df?Pop2000
+df?Density <- df?Pop2010 / df?Surface
+df?Growth <- (df?Pop2010 - df?Pop2000) / df?Pop2000
  
 let map2 = R.joinCountryData2Map(df,"ISO3","Codes")
 R.mapCountryData(map2,"Density")
