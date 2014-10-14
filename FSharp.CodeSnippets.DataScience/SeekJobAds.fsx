60c6f19aac8db80a6fa713a8cfb7e54fc047cd44
stuartjdavies <stuart_j_davies@hotmail.com>
2014-10-15 00:59:25 +1100

    Removed code from Seek

--- FSharp.CodeSnippets.DataScience/SeekJobAds.fsx
+++ FSharp.CodeSnippets.DataScience/SeekJobAds.fsx
@@ -16,16 +16,3 @@ open FSharp.ExcelProvider
 open Microsoft.FSharp.Data.TypeProviders
 open System.Linq
 open System.Data
-
-//
-// Statistics of Highly skilled visas approved in 2014
-//
-let filePath = @"C:\Users\stuart\Documents\GitHub\FSharp.CodeSnippets\FSharp.CodeSnippets.Data\FA140301378-3.xls"
-type ApplicationsSchema = ExcelFile<"C:\Users\stuart\Documents\GitHub\FSharp.CodeSnippets\FSharp.CodeSnippets.Data\FA140301378-3.xls", "2012-13!A8:F65536">
-
-let applications = new ApplicationsSchema(filePath)
-
-let applicationsApprovedByIndustry = applications.Data |> Seq.filter(fun r -> r.``Nomination Approved``.CompareTo("Y") = 0)
-                                                       |> Seq.groupBy(fun r -> r.``Sponsor Industry (self identified)``)
-                                                       |> Seq.map(fun (g, rows) -> (g, (rows |> Seq.length)))
-                                                       |> Seq.sortBy(fun (g, l) -> -l)
