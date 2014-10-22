91029ab9cff8c16e0422ce3c4605c15f01fb8a0c
stuartjdavies <stuart_j_davies@hotmail.com>
2014-10-22 17:34:00 +1100

    Added service bus

---
+++ FSharp.CodeSnippets.Azure/Azure_ServiceBus_Queues.fsx
@@ -0,0 +1,37 @@
+﻿#r @"..\packages\WindowsAzure.ServiceBus.2.4.6.0\lib\net40-full\Microsoft.ServiceBus.dll" 
+#r "System.Runtime.Serialization.dll"
+ 
+open System
+open System.IO
+open System.Linq
+open Microsoft.ServiceBus
+open Microsoft.ServiceBus.Messaging
+
+let connectionString = @"Endpoint=sb://stuarttestservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mhub6BPxFjKiBT9ovNBlbln5SD2U2MyeeUsZJ0lCZWo="
+let queueName = "TestQueue"
+
+let namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+let CreateQueueIfNotExists qName = if (namespaceManager.QueueExists(qName) = false) then
+                                        namespaceManager.CreateQueue(qName) |> ignore                                        
+                                        true
+                                   else
+                                        false                                        
+
+CreateQueueIfNotExists queueName
+
+let client = QueueClient.CreateFromConnectionString(connectionString, queueName)
+
+seq { 0 .. 20 } |> Seq.iter(fun n -> client.Send(new BrokeredMessage(String.Format("Message - {0}", n))))
+
+client.ReceiveBatch(40) |> Seq.iter(fun m -> try
+                                               printfn "Recieved message %s" (m.GetBody<string>())
+                                               m.Complete()
+                                             with
+                                             | e -> printfn "Exception receiving message - %s" e.Message 
+                                                    m.Abandon())
+                                                     
+                                                            
+
+
+
---
+++ FSharp.CodeSnippets.Azure/Azure_ServiceBus_Topics.fsx
@@ -0,0 +1,39 @@
+﻿#r @"..\packages\WindowsAzure.ServiceBus.2.4.6.0\lib\net40-full\Microsoft.ServiceBus.dll" 
+#r "System.Runtime.Serialization.dll"
+ 
+open System
+open System.IO
+open System.Linq
+open Microsoft.ServiceBus
+open Microsoft.ServiceBus.Messaging
+
+let connectionString = @"Endpoint=sb://stuarttestservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mhub6BPxFjKiBT9ovNBlbln5SD2U2MyeeUsZJ0lCZWo="
+let topic = "TestTopic"
+
+// Under construction
+
+
+let namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+let CreateTopicIfNotExists t = if (namespaceManager.TopicExists(t) = false) then
+                                 let newTopic = namespaceManager.CreateTopic(t) 
+                                 namespaceManager.CreateSubscription(newTopic.Path, "topic1") |> ignore
+                                 namespaceManager.CreateSubscription(newTopic.Path, "topic2") |> ignore                                                                  
+                                 true
+                               else
+                                 false                                        
+
+
+let client = TopicClient.CreateFromConnectionString(connectionString, topic)
+seq { 0 .. 20 } |> Seq.iter(fun n -> client.Send(new BrokeredMessage(String.Format("Message - {0}", n))))
+
+
+
+
+
+client.ReceiveBatch(40) |> Seq.iter(fun m -> try
+                                               printfn "Recieved message %s" (m.GetBody<string>())
+                                               m.Complete()
+                                             with
+                                             | e -> printfn "Exception receiving message - %s" e.Message 
+                                                    m.Abandon())
