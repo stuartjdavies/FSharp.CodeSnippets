967c02f50e3e8e4599741ce45398b017dda593ae
stuartjdavies <stuart_j_davies@hotmail.com>
2014-11-02 21:58:00 +1100

    Added computational expressions and aws tables

---
+++ FSharp.CodeSnippets.AWS/DynamoDB_WorkingWithTables.fsx
@@ -0,0 +1,104 @@
+﻿#r "../packages/AWSSDK.2.3.5.0/lib/net45/AWSSDK.dll"
+
+open System
+open System.Collections.Generic
+open Amazon.DynamoDBv2
+open Amazon.DynamoDBv2.DocumentModel
+open Amazon.DynamoDBv2.Model
+open Amazon.Runtime
+open Amazon.SecurityToken
+
+let accessKeyId=""
+let secretAccessKeyID=""
+
+let client = new AmazonDynamoDBClient(accessKeyId,secretAccessKeyID,Amazon.RegionEndpoint.APSoutheast2) 
+let tableName = "ExampleTable"; 
+
+let WaitUntilTableReady(tableName) =        
+        let rec aux(tblName) =              
+                    // Let us wait until table is created. Call DescribeTable.
+                    System.Threading.Thread.Sleep(5000) // Wait 5 seconds.
+                    try                
+                        let res = client.DescribeTable(request=new DescribeTableRequest(TableName=tableName))
+                    
+                        printf "Table name: %s, status: %s" res.Table.TableName (res.Table.TableStatus.ToString())
+                                                                  
+                        if (res.Table.TableStatus = TableStatus.ACTIVE) then
+                           res.Table.TableStatus
+                        else
+                          aux(tblName)                                              
+                    with
+                    // DescribeTable is eventually consistent. So you might
+                    // get resource not found. So we handle the potential exception.                    
+                    | :? ResourceNotFoundException as re -> aux(tblName)         
+        aux(tableName)
+                                                     
+let CreateExampleTable(tableName) =
+        printfn "\n*** Creating table ***"
+       
+        let request = new CreateTableRequest(AttributeDefinitions = List<AttributeDefinition> [                
+                                                new AttributeDefinition(AttributeName = "Id",AttributeType= ScalarAttributeType.op_Implicit "N");
+                                                new AttributeDefinition(AttributeName = "ReplyDateTime", AttributeType= ScalarAttributeType.op_Implicit "N")],
+                                             KeySchema = List<KeySchemaElement> [new KeySchemaElement(AttributeName = "Id", KeyType = KeyType.op_Implicit "HASH");
+                                                                                 new KeySchemaElement(AttributeName = "ReplyDateTime", KeyType = KeyType.op_Implicit "RANGE")],                                             
+                                             ProvisionedThroughput = new ProvisionedThroughput(ReadCapacityUnits = (int64 5), WriteCapacityUnits = (int64 6)),
+                                             TableName=tableName)
+
+        let response = client.CreateTable(request)
+
+        let tableDescription = response.TableDescription
+        printf "%s: %s \t ReadsPerSec: %d \t WritesPerSec: %d"
+                            (tableDescription.TableStatus.ToString())
+                            tableDescription.TableName
+                            tableDescription.ProvisionedThroughput.ReadCapacityUnits
+                            tableDescription.ProvisionedThroughput.WriteCapacityUnits
+
+        let status = tableDescription.TableStatus
+        printfn "%s - %s" tableName (status.ToString())
+
+        WaitUntilTableReady(tableName)
+
+let GetTableNames() =
+        let rec aux(lastTableNameEvaluated, acc) =
+                let request = new ListTablesRequest(Limit = 2, ExclusiveStartTableName = lastTableNameEvaluated)                
+
+                let response = client.ListTables(request)                                        
+                        
+                if (response.LastEvaluatedTableName = null) then
+                    acc   
+                else                         
+                    aux(response.LastEvaluatedTableName, (acc @ (response.TableNames |> List.ofSeq)))                                                  
+        aux(null, List.Empty)       
+
+let GetTableInformation(tableName) =  
+        new DescribeTableRequest(TableName=tableName) |> client.DescribeTable |> (fun r -> r.Table)
+
+let UpdateExampleTable(tableName) =          
+       new UpdateTableRequest(TableName = tableName, 
+                              ProvisionedThroughput=new ProvisionedThroughput(ReadCapacityUnits=6,WriteCapacityUnits=7))
+       |> client.UpdateTable |> ignore            
+       WaitUntilTableReady(tableName)
+       
+let DeleteTable(tableName) = new DeleteTableRequest(TableName=tableName) |> client.DeleteTable                       
+        
+printfn "\n*** listing tables ***"   
+GetTableNames() |> List.iter(fun tn -> printfn "%s" tn)             
+
+printfn "\n*** Retrieving table information ***"
+
+GetTableInformation(tableName) 
+|> (fun description -> printfn "Name: %s" description.TableName
+                       printfn "# of items: %d" description.ItemCount
+                       printfn "Provision Throughput (reads/sec): %d" description.ProvisionedThroughput.ReadCapacityUnits
+                       printfn "Provision Throughput (writes/sec): %d" description.ProvisionedThroughput.WriteCapacityUnits
+                       ())
+
+printfn "\n*** Updating table ***"
+
+UpdateExampleTable tableName
+
+printfn "*** Deleting table ***"
+
+DeleteTable tableName
+
+printfn "Table is being deleted..."        
---
+++ FSharp.CodeSnippets/ComputaionalExpressions.fsx
@@ -0,0 +1,47 @@
+﻿type Result<'A> = | Success of 'A 
+                  | ErrorMessage of string
+                  | Exception of System.Exception
+                  | ErrorCode of int
+
+type AttemptBuilder() =
+        member this.Bind(x, f) = 
+                    match(x) with
+                    | Success x -> f(x) 
+                    | ErrorMessage x -> ErrorMessage x 
+                    | Exception x -> Exception x 
+                    | ErrorCode x -> ErrorCode x
+        member this.Delay(f) = f()
+        member this.Return(x) = Success x
+
+let attempt = new AttemptBuilder()
+
+let Operation1() = Success("Operation 1 Success")
+let Operation2() = Success("Operation 2 Success")
+let Operation3() = ErrorMessage("Operation 3 Failure")
+let Operation4() = Exception(new System.Exception("TestException"))
+
+// Success Attempt
+let result1 = attempt {
+                let! r1 = Operation1()
+                let! r2 = Operation2() 
+
+                return r2   
+              }
+
+// Failure Attempt
+let result2 = attempt {
+                let! r1 = Operation1()
+                let! r2 = Operation2() 
+                let! r3 = Operation3() 
+
+                return r1   
+              }
+
+let PrintResult result = match(result) with
+                         | Success r -> printfn "Success value - %s" r
+                         | ErrorMessage r -> printfn "Failure value - %s" r
+                         | Exception r -> printfn "Exception failure - %s" (r.Message)
+                         | ErrorCode r -> printfn "Exception failure - %d" r
+
+PrintResult result1
+PrintResult result2
