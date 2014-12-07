b9c45350fdca1ca4e3e23b57f84a1159985d38bd
stuartjdavies <stuart_j_davies@hotmail.com>
2014-12-07 20:10:31 +1100

    Added Directory Search

---
+++ FSharp.CodeSnippets.Parallel/AusBankStockPrices.fsx
@@ -0,0 +1,47 @@
+﻿#load "../packages/FsLab.0.0.19/FsLab.fsx"
+#r "../packages/FSharp.Collections.ParallelSeq.1.0.2/lib/net40/FSharp.Collections.ParallelSeq.dll"
+
+open System.Net
+open Microsoft.FSharp.Control.WebExtensions
+open FSharp.Data
+open FSharp.Charting
+open FSharp.Collections.ParallelSeq
+
+let stocks = [ ("Westpac", "http://ichart.finance.yahoo.com/table.csv?s=WBC.AX"); 
+               ("Commonwealth Bank", "http://ichart.finance.yahoo.com/table.csv?s=CBA.AX"); 
+               ("ANZ", "http://ichart.finance.yahoo.com/table.csv?s=ANZ.AX");
+               ("Bendigo And Adelaide Bank Ltd", "http://ichart.finance.yahoo.com/table.csv?s=BEN.AX");
+               ("Macquarie Group Limited", "http://ichart.finance.yahoo.com/table.csv?s=MQG.AX")]
+
+type StockSchema = CsvProvider<"http://ichart.finance.yahoo.com/table.csv?s=WBC.AX">               
+
+let GetExecTime(f : (unit -> unit)) =
+        let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+        f()
+        stopwatch.Stop()
+        stopwatch.Elapsed
+
+let seqTime = GetExecTime (fun () -> stocks |> Seq.map (fun (name,url) -> (name, Http.RequestString(url)))
+                                            |> Seq.map (fun (name, data) -> let stock = StockSchema.Parse(data)
+                                                                            (name, [ for row in stock.Rows -> row.Date, row.Open]))
+                                            |> Seq.iter(fun (name, _) -> printfn "Seq Downlaoded %s" name))
+
+
+let asyncTime = GetExecTime (fun () -> seq { for s in stocks -> 
+                                                async { return (fst(s), ([for row in StockSchema.Parse(Http.RequestString(snd(s))).Rows -> row.Date, row.Open]))}}
+                                       |> Async.Parallel
+                                       |> Async.RunSynchronously
+                                       |> Seq.iter(fun (name, _) -> printfn "Async Downlaoded %s" name))                       
+
+let pseqTime = GetExecTime (fun () -> stocks |> PSeq.map (fun (name,url) -> (name, Http.RequestString(url)))
+                                             |> PSeq.map (fun (name, data) -> let stock = StockSchema.Parse(data)
+                                                                              (name, [ for row in stock.Rows -> row.Date, row.Open]))
+                                             |> Seq.iter(fun (name, _) -> printfn "PSeq Downlaoded %s" name))
+
+
+printfn "-------------------------------------------------\r"
+[("PSeq", pseqTime.TotalMilliseconds);
+ ("Seq", seqTime.TotalMilliseconds);
+ ("Async", asyncTime.TotalMilliseconds)]
+|> Chart.Bar
+
---
+++ FSharp.CodeSnippets.Parallel/DirectorySearch.fsx
@@ -0,0 +1,55 @@
+﻿#load "../packages/FsLab.0.0.19/FsLab.fsx"
+#r "../packages/FSharp.Collections.ParallelSeq.1.0.2/lib/net40/FSharp.Collections.ParallelSeq.dll"
+
+open System.IO
+open FSharp.Collections.ParallelSeq
+open FSharp.Charting
+open System.Linq
+
+let rec GetFiles(path) =                                                            
+          ((Directory.GetFiles(path) |> Array.toSeq), (Directory.GetDirectories(path) |> Seq.collect(fun d -> GetFiles(d)))) 
+          ||> Seq.append 
+
+let rec GetFilesPSeq(path) =                                                                      
+          ((Directory.GetFiles(path) |> Array.toSeq), (Directory.GetDirectories(path) |> PSeq.collect(fun d -> GetFilesPSeq(d)))) 
+          ||> PSeq.append
+          
+let rec GetFilesArray(path) =                                                                      
+          ((Directory.GetFiles(path)), (Directory.GetDirectories(path) |> Array.collect(fun d -> GetFilesArray(d)))) 
+          ||> Array.append
+  
+              
+let path="""C:\Program Files (x86)"""
+
+let GetExecTime(f : (unit -> unit)) =
+        let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+        f()
+        stopwatch.Stop()
+        stopwatch.Elapsed
+
+let seqTime = GetExecTime (fun () -> GetFiles(path)
+                                     |> Seq.filter(fun fileName -> FileInfo(fileName).Length > 100000000L)
+                                     |> Seq.iter(fun fileName -> printfn "FileName - %s Bytes - %d"  fileName (FileInfo(fileName).Length)))
+
+let pseqTime = GetExecTime (fun () -> GetFiles(path)
+                                      |> PSeq.filter(fun fileName -> FileInfo(fileName).Length > 100000000L)
+                                      |> PSeq.iter(fun fileName -> printfn "FileName - %s Bytes - %d"  fileName (FileInfo(fileName).Length)))
+
+let arrayTime = GetExecTime (fun () -> GetFilesArray(path)
+                                       |> Array.filter(fun fileName -> FileInfo(fileName).Length > 100000000L)
+                                       |> Array.iter(fun fileName -> printfn "FileName - %s Bytes - %d"  fileName (FileInfo(fileName).Length)))
+
+let plinqTime = GetExecTime (fun () -> GetFilesArray(path).AsParallel()
+                                                          .Where(fun fileName -> FileInfo(fileName).Length > 100000000L)
+                                                          .ToList().ForEach(fun fileName -> printfn "FileName - %s Bytes - %d"  fileName (FileInfo(fileName).Length)))
+
+let linqTime = GetExecTime (fun () -> GetFilesArray(path).Where(fun fileName -> FileInfo(fileName).Length > 100000000L)
+                                                         .ToList().ForEach(fun fileName -> printfn "FileName - %s Bytes - %d"  fileName (FileInfo(fileName).Length)))
+
+
+[("PSeq", pseqTime.TotalMilliseconds);
+ ("Seq", seqTime.TotalMilliseconds);
+ ("Array", arrayTime.TotalMilliseconds);
+ ("PLinq", plinqTime.TotalMilliseconds);
+ ("Linq", plinqTime.TotalMilliseconds)]
+|> Chart.Bar
